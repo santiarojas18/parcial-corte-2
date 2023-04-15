62fd5248e61fc6c6f5ad97fccd470c93cef17880
santiarojas18 <123812766+santiarojas18@users.noreply.github.com>
2023-04-15 09:07:33 -0500

    Delete demo directory

--- demo/src/main/java/com/juanSanti/demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.juanSanti.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.juanSanti.demo.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
